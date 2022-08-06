aad5b8a92aebaaea3ede523d704150dacefd9703
Will <will@containerlabs.io>
2022-08-05 18:02:49 -0700

    massdriver yaml cleaner output

--- kubernetes-job/src/main.tf
+++ kubernetes-job/src/main.tf
@@ -1,17 +1,11 @@
-<md if (eq .CloudProvider "aws") md>
-module "k8s_application" {
-  source = "massdriver......./..../k8s-application-aws?ref=TODO"
-}
-<md end md>
+# module "k8s_application" {
+#   source = "massdriver......./..../k8s-application-aws?ref=TODO"
+# }
 
-<md if (eq .CloudProvider "azure") md>
-module "k8s_application" {
-  source = "massdriver......./..../k8s-application-azure?ref=TODO"
-}
-<md end md>
+# module "k8s_application" {
+#   source = "massdriver......./..../k8s-application-azure?ref=TODO"
+# }
 
-<md if (eq .CloudProvider "gcp") md>
-module "k8s_application" {
-  source = "github.com/massdriver-cloud/terraform-modules//k8s-application-gcp?ref=TODO"
-}
-<md end md>
+# module "k8s_application" {
+#   source = "github.com/massdriver-cloud/terraform-modules//k8s-application-gcp?ref=TODO"
+# }
