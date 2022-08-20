aa9ee4b032206eaf3a3e272a0a8056909315f305
Cory O'Daniel <cory@coryodaniel.com>
2022-08-20 15:59:40 -0700

    remove todos

--- kubernetes-deployment/src/main.tf
+++ kubernetes-deployment/src/main.tf
@@ -1,8 +1,6 @@
 # TODO: chart name comes from var.md_metadata.name_prefix
-# TODO: feed env vars to helm chart from module.application.envs (and merge w/ param.envs if we want to keep this, im not a fan)
 # TODO: make sure the 'host' field is actually just the subdomain, k8s will be handling the zone. Make this clear in the description
 # TODO: I dont see any external-dns in the chart...
-# TODO: add helm resource
 
 module "application" {
   source = "github.com/massdriver-cloud/terraform-modules//massdriver-application"
