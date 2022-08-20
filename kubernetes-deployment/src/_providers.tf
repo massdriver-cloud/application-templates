a501a5eafa13dffb78f16e675a4d2604d8405704
Cory O'Daniel <cory@coryodaniel.com>
2022-08-20 15:58:21 -0700

    working through it

--- kubernetes-deployment/src/_providers.tf
+++ kubernetes-deployment/src/_providers.tf
@@ -14,30 +14,28 @@ terraform {
   }
 }
 
-provider "mdxc" {
-  azure = null
-  gcp   = null
-  aws = {
-    # TODO: need to ge this dynamically
-    # NBD, this will be in the provider.tf file for each template. In k8s we cna reach from the cluster, in labmda, etc we can read from params.region or vpc
-    region = "us-west-2"
-
-    # we'll ned a bunch of logic to figure out the auth in the template.
-    role_arn    = var.aws_authentication.data.arn
-    external_id = var.aws_authentication.data.external_id
-  }
+locals {
+  # Note: If the field name `kubernetes_cluster` in massdriver.yaml has been changed, these must be updated to match.
+  kubernetes_cluster_auth = var.kubernetes_cluster.data.authentication
+  kubernetes_cloud        = var.kubernetes_cluster.specs.kubernetes.cloud
 }
 
-# TODO: sniff this from kubernetes_cluster
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#     # token                  = local.kubernetes_cluster.data.authentication.user.token
-#     # this returns a value that doesn't work for the cluster
-#     # I'm not sure if it's the creds I used in the AWS provider
-#     # but I tried to use the ones that made the cluster
-#     # in GCP, we use all fields from the artifact, what are your thoughts on this?
-#     token = data.aws_eks_cluster_auth.auth.token
-#   }
-# }
+provider "mdxc" {
+  # TODO: auth will still be borked as the fields may not be present in, we'll need to use a lot of lookups
+  azure = local.kubernetes_cloud == "azure" ? null : null
+  gcp   = local.kubernetes_cloud == "gcp" ? null : null
+  aws = local.kubernetes_cloud == "aws" ? {
+    # TODO: need to ge this dynamically
+    region      = "us-west-2"
+    role_arn    = var.aws_authentication.data.arn
+    external_id = var.aws_authentication.data.external_id
+  } : null
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = local.kubernetes_cluster_auth.cluster.server
+    cluster_ca_certificate = base64decode(local.kubernetes_cluster_auth.certificate-authority-data)
+    token                  = local.kubernetes_cluster_auth.user.token
+  }
+}
