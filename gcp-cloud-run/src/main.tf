2bba784935e5f4618e7ffaaec616b854e23ccefd
Will <will@containerlabs.io>
2022-08-29 16:31:50 -0700

    trimmed cloud run, removed cloud function

--- gcp-cloud-run/src/main.tf
+++ gcp-cloud-run/src/main.tf
@@ -1,15 +1,6 @@
-module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application?ref=c879d33"
-  name    = var.md_metadata.name_prefix
-  service = "function"
-}
-
-module "cloud_run" {
-  source               = "./module"
-  application_identity = module.application.id
-  md_metadata          = var.md_metadata
-  container_image      = "${var.container.repository}/${var.container.image}:${var.container.tag}"
-  envs                 = module.application.envs
-  endpoint             = var.endpoint
-  location             = var.location
+module "application_cloud_run" {
+  source          = "github.com/massdriver-cloud/terraform-modules//gcp-application-cloud-run"
+  location        = var.location
+  container_image = "${var.container.repository}/${var.container.image}:${var.container.tag}"
+  endpoint        = var.endpoint
 }
