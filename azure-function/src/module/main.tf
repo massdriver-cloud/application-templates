ae76f4ee26b3ddd7254aac57b8b7ac1f05002b75
Will <will@containerlabs.io>
2022-08-28 19:06:06 -0700

    azure misc

--- azure-function/src/module/main.tf
+++ azure-function/src/module/main.tf
@@ -1,6 +1,5 @@
 data "azurerm_resource_group" "main" {
-  name     = var.azurerm_resource_group
-  location = var.location
+  name = var.resource_group_name
 }
 
 resource "azurerm_linux_function_app" "main" {
@@ -11,15 +10,15 @@ resource "azurerm_linux_function_app" "main" {
     // module.application.....
     identity_ids = [azurerm_user_assigned_identity.example.id]
   }
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
 
   # optional
-  storage_account_name = azurerm_storage_account.example.name
+  storage_account_name = azurerm_storage_account.main.name
   # optional
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
   # required
-  service_plan_id = azurerm_service_plan.example.id
+  service_plan_id = azurerm_service_plan.main.id
 
   docker {
     image_name = var.container_repository.name
@@ -33,18 +32,18 @@ resource "azurerm_linux_function_app" "main" {
   site_config {}
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "linuxfunctionappsa"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
+resource "azurerm_storage_account" "main" {
+  name                     = var.md_metadata.name_prefix
+  resource_group_name      = data.azurerm_resource_group.main.name
+  location                 = data.azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example-app-service-plan"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+resource "azurerm_service_plan" "main" {
+  name                = var.md_metadata.name_prefix
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
   os_type             = "Linux"
   sku_name            = "Y1"
 }
