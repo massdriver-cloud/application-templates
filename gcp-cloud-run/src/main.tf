4b6f7bed2a8d49db74878bce3c4bee261be089c4
Will Beebe <will@containerlabs.io>
2022-08-30 10:45:48 -0700

    Update gcp-cloud-run/src/main.tf

--- gcp-cloud-run/src/main.tf
+++ gcp-cloud-run/src/main.tf
@@ -1,5 +1,9 @@
 module "application_cloud_run" {
+<<<<<<< HEAD
   source          = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=2b0f322"
+=======
+  source          = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run"
+>>>>>>> 1548851 (Update gcp-cloud-run/src/main.tf)
   location        = var.location
   container_image = "${var.container.repository}/${var.container.image}:${var.container.tag}"
   endpoint        = var.endpoint
