940fc4e8756529b22594bc41d8fb19d10f9fed9b
Will <will@containerlabs.io>
2022-09-06 13:20:49 -0700

    no cloud function

--- gcp-cloud-function/src/main.tf
+++
@@ -1,9 +0,0 @@
-module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application"
-  name    = var.md_metadata.name_prefix
-  service = "function"
-}
-
-# TODO: Put gcp function tf code here
-
-
