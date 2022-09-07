35e088624b54b8b55031df23b49fc278a1ad54c1
Will <will@containerlabs.io>
2022-09-06 17:05:34 -0700

    yank cloud run for now

--- gcp-cloud-run/src/apis.tf
+++
@@ -1,7 +0,0 @@
-module "apis" {
-  source = "github.com/massdriver-cloud/terraform-modules//google-enable-apis?ref=9201b9f"
-  services = [
-    "cloudbuild.googleapis.com",
-    "run.googleapis.com"
-  ]
-}
