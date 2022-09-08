e4e43fcfde1dc3e7cf163b96f4a003068be708d0
Will <will@containerlabs.io>
2022-09-07 18:22:41 -0700

    adding vars

--- gcp-cloud-run/src/main.tf
+++ gcp-cloud-run/src/main.tf
@@ -1,5 +1,6 @@
 module "application_cloud_run" {
 <<<<<<< HEAD
+<<<<<<< HEAD
   source          = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=2b0f322"
 =======
   source          = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run"
@@ -7,4 +8,13 @@ module "application_cloud_run" {
   location        = var.location
   container_image = "${var.container.repository}/${var.container.image}:${var.container.tag}"
   endpoint        = var.endpoint
+=======
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=2b0f322"
+  location           = var.location
+  container_image    = "${var.container.repository}/${var.container.image}:${var.container.tag}"
+  zone               = local.zone
+  subdomain          = local.subdomain
+  network            = var.gcp_global_network.data.grn
+  vpc_connector_cidr = var.private_service_cidr
+>>>>>>> 65274e2 (adding vars)
 }
