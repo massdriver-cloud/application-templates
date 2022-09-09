661393f7e50be6d485720d4cd2c192d2c8cacc0a
Will <will@containerlabs.io>
2022-09-08 17:51:48 -0700

    adding titles, cleanup terraform

--- gcp-cloud-run/src/main.tf
+++ gcp-cloud-run/src/main.tf
@@ -12,8 +12,8 @@ module "application_cloud_run" {
   source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=2b0f322"
   location           = var.location
   container_image    = "${var.container.repository}/${var.container.image}:${var.container.tag}"
-  zone               = local.zone
-  subdomain          = local.subdomain
+  zone               = var.endpoint.enabled ? var.endpoint.zone : null
+  subdomain          = var.endpoint.enabled ? var.endpoint.subdomain : null
   network            = var.gcp_global_network.data.grn
   vpc_connector_cidr = var.private_service_cidr
 >>>>>>> 65274e2 (adding vars)
