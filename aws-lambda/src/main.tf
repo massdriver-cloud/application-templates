11c81a6da35f1af84bb66665f4376c626531e1f6
dwill <xpositivityx@gmail.com>
2022-09-12 21:30:52 -0700

    lambda application template

--- aws-lambda/src/main.tf
+++ aws-lambda/src/main.tf
@@ -1,7 +1,9 @@
-module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application"
-  name    = var.md_metadata.name_prefix
-  service = "function"
+module "lambda_application" {
+  source            = "github.com/massdriver-cloud/terraform-modules//massdriver-application-aws-lambda"
+  md_metadata       = var.md_metadata
+  image             = var.runtime.image
+  x_ray_enabled     = var.observability.x-ray.enabled
+  retention_days    = var.observability.retention_days
+  memory_size       = var.runtime.memory_size
+  execution_timeout = var.runtime.execution_timeout
 }
-
-# TODO: Put aws lambda function tf code here
