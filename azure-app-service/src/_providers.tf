e61605d23ceaab56cdcffd512545a3a6db927e36
mclacore <michael@massdriver.cloud>
2022-09-14 12:10:27 -0600

    App template deploying locally

--- azure-app-service/src/_providers.tf
+++ azure-app-service/src/_providers.tf
@@ -16,18 +16,18 @@ terraform {
 
 provider "mdxc" {
   azure = {
-    client_id       = var.azure_authentication.data.client_id
-    tenant_id       = var.azure_authentication.data.tenant_id
-    client_secret   = var.azure_authentication.data.client_secret
-    subscription_id = var.azure_authentication.data.subscription_id
+    client_id       = var.azure_service_principal.data.client_id
+    tenant_id       = var.azure_service_principal.data.tenant_id
+    client_secret   = var.azure_service_principal.data.client_secret
+    subscription_id = var.azure_service_principal.data.subscription_id
   }
 }
 
 provider "azurerm" {
   features {}
 
-  client_id       = var.azure_authentication.data.client_id
-  tenant_id       = var.azure_authentication.data.tenant_id
-  client_secret   = var.azure_authentication.data.client_secret
-  subscription_id = var.azure_authentication.data.subscription_id
+  client_id       = var.azure_service_principal.data.client_id
+  tenant_id       = var.azure_service_principal.data.tenant_id
+  client_secret   = var.azure_service_principal.data.client_secret
+  subscription_id = var.azure_service_principal.data.subscription_id
 }
--- azure-app-service/src/main.tf
+++ azure-app-service/src/main.tf
@@ -1,6 +1,9 @@
 module "application_app_service" {
-  source      = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=edc2f7d"
-  application = var.application
-  image       = var.image
-  dns         = var.dns
+  source        = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service"
+  name          = var.md_metadata.name_prefix
+  application   = var.application
+  image         = var.image
+  dns           = var.dns
+  contact_email = var.md_metadata.target.contact_email
+  tags          = var.md_metadata.default_tags
 }
