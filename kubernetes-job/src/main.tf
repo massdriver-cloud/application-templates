243ef5fc1529dbe2bcac3e3c2606242ed093af8b
chrisghill <chris@massdriver.cloud>
2022-09-17 17:33:23 -0600

    Adding kubernetes job and cronjob templates

---
+++ kubernetes-job/src/main.tf
@@ -0,0 +1,8 @@
+
+module "helm" {
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm"
+  name               = var.md_metadata.name_prefix
+  namespace          = var.namespace
+  chart              = "${path.module}/chart"
+  kubernetes_cluster = var.kubernetes_cluster
+}
