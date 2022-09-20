847fb11040ab72aa52047767d86245de8bef829e
chrisghill <chris@massdriver.cloud>
2022-09-20 10:36:29 -0600

    updates from PR

--- kubernetes-job/src/main.tf
+++ kubernetes-job/src/main.tf
@@ -1,9 +1,9 @@
 
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=99e7509"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=8892201"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "${path.module}/chart"
   kubernetes_cluster = var.kubernetes_cluster
-  additionalEnvs     = var.additionalEnvs
+  additional_envs    = var.additionalEnvs
 }
