ac0e4fb71166cc831dbcbc1286aac137ab081cf2
chrisghill <chris@massdriver.cloud>
2022-09-19 21:14:03 -0600

    Switching to additionalEnvs

--- kubernetes-deployment/src/main.tf
+++ kubernetes-deployment/src/main.tf
@@ -1,7 +1,8 @@
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=99e7509"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "${path.module}/chart"
   kubernetes_cluster = var.kubernetes_cluster
+  additionalEnvs     = var.additionalEnvs
 }
--- kubernetes-job/src/main.tf
+++ kubernetes-job/src/main.tf
@@ -1,8 +1,9 @@
 
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=99e7509"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "${path.module}/chart"
   kubernetes_cluster = var.kubernetes_cluster
+  additionalEnvs     = var.additionalEnvs
 }
