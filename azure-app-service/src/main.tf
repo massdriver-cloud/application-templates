45d3b93be9e7c41396c41828a19432a2eb304b89
Michael Lacore <michael@massdriver.cloud>
2022-09-26 10:18:40 -0600

    Fix Enable DNS bool required error (#45)
    * Fix Enable DNS bool required error
    
    * Update SHA from module

--- azure-app-service/src/main.tf
+++ azure-app-service/src/main.tf
@@ -1,5 +1,5 @@
 module "application_app_service" {
-  source        = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=00aada7"
+  source        = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=4018386"
   name          = var.md_metadata.name_prefix
   application   = var.application
   image         = var.image
