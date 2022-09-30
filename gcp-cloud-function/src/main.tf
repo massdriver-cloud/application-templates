ba7608a49d4abd52e3cc50674c60edacb890c715
Will <will@containerlabs.io>
2022-09-29 19:36:49 -0700

    tf plan working

--- gcp-cloud-function/src/main.tf
+++ gcp-cloud-function/src/main.tf
@@ -1,9 +1,16 @@
 module "app" {
-  source                       = "../../../terraform-modules/massdriver-application-gcp-cloud-function"
-  location                     = var.subnetwork.specs.gcp.region
-  runtime                      = var.runtime
-  source_archive               = var.source_archive
+  source   = "../../../terraform-modules/massdriver-application-gcp-cloud-function"
+  name     = var.md_metadata.name_prefix
+  location = var.subnetwork.specs.gcp.region
+  runtime  = var.runtime
+  source_archive = {
+    bucket = var.source_bucket.data.infrastructure.id
+    object = var.source_archive_path
+  }
   cloud_function_configuration = var.cloud_function_configuration
   endpoint                     = var.endpoint
-  subnetwork                   = var.subnetwork.data.infrastructure.grn
+}
+
+variable "source_bucket" {
+  type = any
 }
