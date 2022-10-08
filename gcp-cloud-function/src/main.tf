37dc521970d88ed934b4da5a066fd05fd733fba3
Will <will@containerlabs.io>
2022-10-07 18:34:48 -0700

    use vpc connector, location from subnet

--- gcp-cloud-function/src/main.tf
+++ gcp-cloud-function/src/main.tf
@@ -1,16 +1,47 @@
-module "app" {
-  source   = "../../../terraform-modules/massdriver-application-gcp-cloud-function"
-  name     = var.md_metadata.name_prefix
-  location = var.subnetwork.specs.gcp.region
-  runtime  = var.runtime
-  source_archive = {
-    bucket = var.source_bucket.data.infrastructure.id
-    object = var.source_archive_path
-  }
-  cloud_function_configuration = var.cloud_function_configuration
-  endpoint                     = var.endpoint
-}
+# https://cloud.google.com/functions/docs/concepts/execution-environment
+# runtime:
+#   type: string
+#   title: Runtime
+#   oneOf:
+#   - title: Node.js 16
+#     const: nodejs16
+#   - title: Node.js 14
+#     const: nodejs14
+#   - title: Python 3.10
+#     const: python310
+#   - title: Python 3.9
+#     const: python39
+#   - title: Python 3.8
+#     const: python38
+#   - title: Go 1.16
+#     const: go116
+#   - title: Go 1.13
+#     const: go113
+#   - title: Java 17
+#     const: java17
+#   - title: Java 11
+#     const: java11
+#   - title: .NET Core 6.0
+#     const: dotnet6
+#   - title: .NET Core 3.1
+#     const: dotnet3
+#   - title: Ruby 3.0
+#     const: ruby30
+#   - title: Ruby 2.7
+#     const: ruby27
+#   - title: PHP 8.1
+#     const: php81
+#   - title: PHP 7.4
+#     const: php74
 
-variable "source_bucket" {
-  type = any
+module "app" {
+  source              = "../../../terraform-modules/massdriver-application-gcp-cloud-function"
+  md_metadata         = var.md_metadata
+  location            = local.gcp_region
+  source_archive_path = var.source_archive_path
+  cloud_function_configuration = merge(var.cloud_function_configuration, {
+    runtime = "php81"
+  })
+  endpoint      = var.endpoint
+  vpc_connector = var.gcp_subnetwork.data.infrastructure.vpc_access_connector
 }
--- gcp-cloud-run/src/main.tf
+++ gcp-cloud-run/src/main.tf
@@ -1,9 +1,8 @@
 module "application_cloud_run" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=96fa197"
-  location           = var.location
-  container_image    = "${var.container.repository}/${var.container.image}:${var.container.tag}"
-  endpoint           = var.endpoint
-  max_instances      = var.max_instances
-  network            = var.gcp_global_network.data.grn
-  vpc_connector_cidr = var.private_service_cidr
+  source          = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=96fa197"
+  location        = local.gcp_region
+  container_image = "${var.container.repository}/${var.container.image}:${var.container.tag}"
+  endpoint        = var.endpoint
+  max_instances   = var.max_instances
+  vpc_connector   = var.gcp_subnetwork.data.infrastructure.vpc_access_connector
 }
