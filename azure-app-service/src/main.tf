56d5d189c487b6e947f107a1318b79e25861089c
mclacore <michael@massdriver.cloud>
2022-10-27 15:22:01 -0600

    VNet integration with App Service

--- azure-app-service/src/main.tf
+++ azure-app-service/src/main.tf
@@ -1,9 +1,11 @@
 module "application_app_service" {
-  source        = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=22d422e"
-  name          = var.md_metadata.name_prefix
-  application   = var.application
-  image         = var.image
-  dns           = var.dns
-  contact_email = var.md_metadata.target.contact_email
-  tags          = var.md_metadata.default_tags
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=22d422e"
+  name               = var.md_metadata.name_prefix
+  application        = var.application
+  image              = var.image
+  dns                = var.dns
+  virtual_network_id = var.azure_virtual_network.data.infrastructure.id
+  location           = var.azure_virtual_network.specs.azure.region
+  contact_email      = var.md_metadata.target.contact_email
+  tags               = var.md_metadata.default_tags
 }
