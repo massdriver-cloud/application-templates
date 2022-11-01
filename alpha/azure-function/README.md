1d312807041edf3a9bdc291b4b2be68a75b2a19a
Will <will@containerlabs.io>
2022-11-01 14:14:11 -0700

    removing azure function from alpha

--- alpha/azure-function/README.md
+++
@@ -1,3 +0,0 @@
-# Azure Function Application Template
-
-This is a base template for running Azure Functions on Massdriver provisioned infrastructure.
--- alpha/azure-function/src/main.tf
+++
@@ -1,7 +0,0 @@
-module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application"
-  name    = var.md_metadata.name_prefix
-  service = "function"
-}
-
-# TODO: Put azure tf function code here
