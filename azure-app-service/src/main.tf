e64a6bf99c1fd151eb7d4557bb72d6543b1fac31
mclacore <michael@massdriver.cloud>
2022-11-28 11:05:55 -0700

    Optional alarms and auto CIDR

--- azure-app-service/src/main.tf
+++ azure-app-service/src/main.tf
@@ -1,9 +1,12 @@
 module "application_app_service" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=22d422e"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=0b16f8f"
   name               = var.md_metadata.name_prefix
   application        = var.application
   image              = var.image
   dns                = var.dns
+  network            = var.network
+  monitoring         = var.monitoring
+  md_metadata        = var.md_metadata
   virtual_network_id = var.azure_virtual_network.data.infrastructure.id
   location           = var.azure_virtual_network.specs.azure.region
   contact_email      = var.md_metadata.target.contact_email
