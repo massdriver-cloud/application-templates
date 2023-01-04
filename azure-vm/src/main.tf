34d67c330bb0845b65076ae3e0bd3599d1dfe4f1
Will <will@containerlabs.io>
2023-01-03 20:10:50 -0800

    adding providers, using location

--- azure-vm/src/main.tf
+++ azure-vm/src/main.tf
@@ -2,10 +2,11 @@ module "application" {
   source             = "../../terraform-modules/massdriver/application-azure-vm"
   md_metadata        = var.md_metadata
   virtual_network_id = var.azure_virtual_network.data.infrastructure.id
-  location           = "westus"
+  location           = var.location
   auto_scaling = {
     enabled = true
   }
+  endpoint = var.endpoint
 
   health_check = {
     path = "/"
