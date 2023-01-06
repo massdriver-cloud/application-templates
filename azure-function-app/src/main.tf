e5547222ed2d89c8c0220e4b8f45e324b4069ff3
Will <will@containerlabs.io>
2023-01-06 15:47:52 -0800

    update app template

--- azure-function-app/src/main.tf
+++ azure-function-app/src/main.tf
@@ -1,6 +1,6 @@
 module "application_app_service" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-function-app?ref=9aab9df"
-  name               = var.md_metadata.name_prefix
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-function-app?ref=718f586"
+  md_metadata        = var.md_metadata
   application        = var.application
   docker             = var.docker
   dns                = var.dns
@@ -8,7 +8,5 @@ module "application_app_service" {
   monitoring         = var.monitoring
   virtual_network_id = var.azure_virtual_network.data.infrastructure.id
   location           = var.azure_virtual_network.specs.azure.region
-  md_metadata        = var.md_metadata
   contact_email      = var.md_metadata.target.contact_email
-  tags               = var.md_metadata.default_tags
 }
