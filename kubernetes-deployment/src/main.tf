5f7ece8024de15751bf8c9fc829e3d91ad9fcf21
Will <will@containerlabs.io>
2023-01-31 09:41:06 -0800

    adding location and resource group name

--- kubernetes-deployment/src/main.tf
+++ kubernetes-deployment/src/main.tf
@@ -1,8 +1,10 @@
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=8892201"
-  name               = var.md_metadata.name_prefix
-  namespace          = var.namespace
-  chart              = "${path.module}/chart"
-  kubernetes_cluster = var.kubernetes_cluster
-  additional_envs    = []
+  source              = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=5dd4e25"
+  name                = var.md_metadata.name_prefix
+  namespace           = var.namespace
+  chart               = "${path.module}/chart"
+  kubernetes_cluster  = var.kubernetes_cluster
+  additional_envs     = []
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
