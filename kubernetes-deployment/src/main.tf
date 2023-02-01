84b1998cd5b3e1173d1a5ce043e9f5602a27fd65
Will <will@containerlabs.io>
2023-01-31 19:44:29 -0800

    updating k8s template

--- kubernetes-deployment/src/main.tf
+++ kubernetes-deployment/src/main.tf
@@ -1,5 +1,5 @@
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=8892201"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=b4401ac"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "${path.module}/chart"
