14acf5ec2d7e8b32c5f58cf8e92c75eb93b9d11a
Will <will@containerlabs.io>
2023-02-17 14:59:49 -0800

    adding concurrency to template

--- gcp-cloud-run/src/main.tf
+++ gcp-cloud-run/src/main.tf
@@ -1,10 +1,11 @@
 module "application_cloud_run" {
-  source          = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=3ee9d5f"
-  md_metadata     = var.md_metadata
-  location        = local.gcp_region
-  container_image = "${var.container.repository}/${var.container.image}:${var.container.tag}"
-  container_port  = 8080
-  endpoint        = var.endpoint
-  max_instances   = var.max_instances
-  vpc_connector   = var.gcp_subnetwork.data.infrastructure.vpc_access_connector
+  source                = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=0346144"
+  md_metadata           = var.md_metadata
+  location              = local.gcp_region
+  container_image       = "${var.container.repository}/${var.container.image}:${var.container.tag}"
+  container_port        = 8080
+  endpoint              = var.endpoint
+  max_instances         = var.max_instances
+  vpc_connector         = var.gcp_subnetwork.data.infrastructure.vpc_access_connector
+  container_concurrency = var.container_concurrency
 }
