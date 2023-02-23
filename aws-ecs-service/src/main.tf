3b0ff7190680333ab4f753745f2bb78505759ebf
Will <will@containerlabs.io>
2023-02-23 12:10:48 -0800

    pull in new GCP VM module

--- aws-ecs-service/src/main.tf
+++ aws-ecs-service/src/main.tf
@@ -1,23 +1,23 @@
 
 module "application" {
-  source = "github.com/massdriver-cloud/terraform-modules//massdriver-application-aws-ecs-service?ref=f0faeeb"
+  source      = "github.com/massdriver-cloud/terraform-modules//massdriver-application-aws-ecs-service?ref=f0faeeb"
   md_metadata = var.md_metadata
   ecs_cluster = var.ecs_cluster
   task_cpu    = var.resources.cpu
   task_memory = ceil(var.resources.memory)
-  logging     = {
-    driver = var.monitoring.logging.destination
+  logging = {
+    driver    = var.monitoring.logging.destination
     retention = lookup(var.monitoring.logging, "retention", null)
   }
   autoscaling = var.autoscaling
   containers = [
     {
-      name = "nginx"
+      name             = "nginx"
       image_repository = var.image.repository
-      image_tag = var.image.tag
-      ports = [ for port in var.ports : {
+      image_tag        = var.image.tag
+      ports = [for port in var.ports : {
         container_port = port.port
-        ingresses = [ for ingress in lookup(port, "ingresses", []) : {
+        ingresses = [for ingress in lookup(port, "ingresses", []) : {
           hostname   = ingress.hostname
           path       = ingress.path
           create_dns = ingress.create_dns
