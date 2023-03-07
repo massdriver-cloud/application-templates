794eb83ee2a197e842ff5fba4b57ba2b1e21f6f6
Andreas Kasprzok <akasprzok@gmail.com>
2023-03-06 21:34:49 -0800

    Remove alpha, provides no value (#85)

--- alpha/aws-ec2/src/main.tf
+++
@@ -1,7 +0,0 @@
-module "application" {
-  source  = "github.com/massdriver-cloud/terraform-modules//massdriver-application"
-  name    = var.md_metadata.name_prefix
-  service = "vm"
-}
-
-# TODO: Put aws ec2 vm tf code here
