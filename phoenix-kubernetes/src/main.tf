43f08b4d76cd92a8ae153f70dc039354a58f2157
Cory O'Daniel <cory@coryodaniel.com>
2023-03-17 15:25:43 -0700

    initial commit phx template

--- phoenix-kubernetes/src/main.tf
+++ phoenix-kubernetes/src/main.tf
@@ -2,26 +2,22 @@ module "helm" {
   source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=e8bc74bed90c2bbadf1dfe2ebb8be949f2f47543"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
-  chart              = "ruby-on-rails"
+  chart              = "elixir-phoenix"
   helm_repository    = "https://massdriver-cloud.github.io/helm-charts/"
-  helm_version       = "0.2.3"
+  helm_version       = "0.1.4"
   kubernetes_cluster = var.kubernetes_cluster
   additional_envs    = []
   helm_additional_values = {
     # # The default command, args, and migration settings are below. Any
     # # values in values.yaml can be overwritten by adding a parameter to your massdriver.yaml
     # # or by hard coding the value here
-    #     command = ["bundle"]
-    #     args    = ["exec", "rails", "server"]
-    #     migration = {
+    # command = ["mix"]
+    # args    = ["phx.server"]
 
-    #       enabled = true
-    #       command = ["bash", "-c"]
-    #       args = [<<ARGS
-    # bundle exec rails db:create;
-    # bundle exec rails db:migrate;
-    # ARGS
-    #       ]
-    #     }
+    # migration = {
+    #   enabled = true
+    #   command = ["mix"]
+    #   args    = ["do", "ecto.create,", "ecto.migrate"]
+    # }
   }
 }
