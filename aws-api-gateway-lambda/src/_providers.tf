31bcb0c54f74262f43212159a730b8b9cba1f3f4
Dave Williams <xpositivityx@gmail.com>
2023-03-21 12:34:05 -0700

    adding api gateway lambda template and updating lambda module ref (#94)
    * adding api gateway lambda template and updating lambda module ref
    
    * fixing liquid templating
    
    * adding appropriate whitespace between fences

---
+++ aws-api-gateway-lambda/src/_providers.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    mdxc = {
+      source = "massdriver-cloud/mdxc"
+    }
+
+    massdriver = {
+      source = "massdriver-cloud/massdriver"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "mdxc" {
+  aws = {
+    region      = var.region
+    role_arn    = var.aws_authentication.data.arn
+    external_id = var.aws_authentication.data.external_id
+  }
+}
+
+provider "aws" {
+  region = var.region
+  assume_role {
+    role_arn    = var.aws_authentication.data.arn
+    external_id = var.aws_authentication.data.external_id
+  }
+  default_tags {
+    tags = var.md_metadata.default_tags
+  }
+}
