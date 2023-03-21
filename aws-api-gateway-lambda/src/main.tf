5f39460060d22b4c1330b4937bad2ee5e86ecf20
dwill <xpositivityx@gmail.com>
2023-03-21 09:58:41 -0700

    adding api gateway lambda template and updating lambda module ref

---
+++ aws-api-gateway-lambda/src/main.tf
@@ -0,0 +1,31 @@
+module "lambda_application" {
+  source            = "github.com/massdriver-cloud/terraform-modules//massdriver-application-aws-lambda?ref=b81fea7"
+  md_metadata       = var.md_metadata
+  image             = var.runtime.image
+  x_ray_enabled     = var.observability.x-ray.enabled
+  retention_days    = var.observability.retention_days
+  memory_size       = var.runtime.memory_size
+  execution_timeout = var.runtime.execution_timeout
+}
+
+resource "aws_api_gateway_resource" "main" {
+  rest_api_id = local.api_id
+  parent_id   = var.api_gateway.data.infrastructure.root_resource_id
+  path_part   = var.api.path
+}
+
+resource "aws_api_gateway_method" "main" {
+  rest_api_id   = local.api_id
+  resource_id   = aws_api_gateway_resource.main.id
+  http_method   = var.api.http_method
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "main" {
+  rest_api_id             = local.api_id
+  resource_id             = aws_api_gateway_resource.main.id
+  http_method             = aws_api_gateway_method.main.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda_application.function_invoke_arn
+}
