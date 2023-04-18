a7e6afe933645adf03ffe3853a3ec333d8f36644
mclacore <michael@massdriver.cloud>
2023-04-18 15:30:41 -0600

    Adding wizard and endpoint

--- azure-function-app/src/main.tf
+++ azure-function-app/src/main.tf
@@ -1,5 +1,5 @@
 module "application" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-function-app?ref=7caf5a6"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-function-app?ref=735929b"
   name               = var.md_metadata.name_prefix
   location           = var.azure_virtual_network.specs.azure.region
   application        = var.application
