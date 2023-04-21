453181d0b93488cc0d3e2a8852b0f42aa4b138ff
Dave Williams <xpositivityx@gmail.com>
2023-04-21 09:49:27 -0700

    adding api gateway fizzbuzz template (#105)

---
+++ aws-lambda-fizzbuzz-example/deployment/_providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    massdriver = {
+      source = "massdriver-cloud/massdriver"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.api_gateway.specs.aws.region
+  assume_role {
+    role_arn    = var.aws_authentication.data.arn
+    external_id = var.aws_authentication.data.external_id
+  }
+  default_tags {
+    tags = var.md_metadata.default_tags
+  }
+}
