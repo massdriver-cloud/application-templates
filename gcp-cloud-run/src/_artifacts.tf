b1183f248884f15f37aed47659ac0659e9f9f257
Michael Lacore <michael@massdriver.cloud>
2023-04-21 11:32:01 -0600

    Updating template for endpoint support (#104)
    * Wizard preset
    
    * Adding endpoint support
    
    * Update description
    
    * Reverting docker image abstraction

---
+++ gcp-cloud-run/src/_artifacts.tf
@@ -0,0 +1,25 @@
+locals {
+  dns_name = var.endpoint.enabled ? var.endpoint.zone.name : null
+  endpoint = var.endpoint.enabled ? "${var.endpoint.subdomain}.${local.dns_name}" : module.application_cloud_run.hostname
+}
+
+resource "massdriver_artifact" "endpoint" {
+  field                = "endpoint"
+  provider_resource_id = module.application_cloud_run.id
+  name                 = "GCP Cloud Run endpoint for ${var.md_metadata.name_prefix}"
+  artifact = jsonencode(
+    {
+      data = {
+        api = {
+          hostname = local.endpoint
+          port     = 443
+        }
+      }
+      specs = {
+        api = {
+          version = "${var.container.image.repository}:${var.container.image.tag}"
+        }
+      }
+    }
+  )
+}
--- gcp-cloud-run/src/main.tf
+++ gcp-cloud-run/src/main.tf
@@ -1,11 +1,18 @@
 module "application_cloud_run" {
-  source                = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=0346144"
-  md_metadata           = var.md_metadata
-  location              = local.gcp_region
-  container_image       = "${var.container.repository}/${var.container.image}:${var.container.tag}"
-  container_port        = 8080
+  source      = "github.com/massdriver-cloud/terraform-modules//massdriver-application-gcp-cloud-run?ref=9829a65"
+  md_metadata = var.md_metadata
+  platform = {
+    location      = local.gcp_region
+    max_instances = var.platform.max_instances
+  }
+  container = {
+    image = {
+      repository = var.container.image.repository
+      tag        = var.container.image.tag
+    }
+    port        = var.container.port
+    concurrency = var.container.concurrency
+  }
   endpoint              = var.endpoint
-  max_instances         = var.max_instances
   vpc_connector         = var.gcp_subnetwork.data.infrastructure.vpc_access_connector
-  container_concurrency = var.container_concurrency
 }
