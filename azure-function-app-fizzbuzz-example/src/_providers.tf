3328c7414d1594c36736628a3cba3ebe4b03b317
Michael Lacore <michael@massdriver.cloud>
2023-04-23 07:09:33 -0600

    Moving azure function fizzbuzz example (#107)

---
+++ azure-function-app-fizzbuzz-example/src/_providers.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    mdxc = {
+      source  = "massdriver-cloud/mdxc"
+      version = ">= 0.10.3"
+    }
+
+    massdriver = {
+      source = "massdriver-cloud/massdriver"
+    }
+
+    utility = {
+      source = "massdriver-cloud/utility"
+    }
+
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "mdxc" {
+  azure = {
+    client_id       = var.azure_service_principal.data.client_id
+    tenant_id       = var.azure_service_principal.data.tenant_id
+    client_secret   = var.azure_service_principal.data.client_secret
+    subscription_id = var.azure_service_principal.data.subscription_id
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+  client_id       = var.azure_service_principal.data.client_id
+  tenant_id       = var.azure_service_principal.data.tenant_id
+  client_secret   = var.azure_service_principal.data.client_secret
+  subscription_id = var.azure_service_principal.data.subscription_id
+}
---
+++ azure-function-app-fizzbuzz-example/src/main.tf
@@ -0,0 +1,21 @@
+module "application" {
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-function-app?ref=735929b"
+  name               = var.md_metadata.name_prefix
+  location           = var.azure_virtual_network.specs.azure.region
+  application        = var.application
+  image              = var.image
+  monitoring         = var.monitoring
+  dns                = var.dns
+  virtual_network_id = var.azure_virtual_network.data.infrastructure.id
+  contact_email      = var.md_metadata.target.contact_email
+  md_metadata        = var.md_metadata
+  tags               = var.md_metadata.default_tags
+
+  network = {
+    auto = true
+  }
+  health_check = {
+    path = "/health"
+    port = 80
+  }
+}
