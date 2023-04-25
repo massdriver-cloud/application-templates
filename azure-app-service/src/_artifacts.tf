bf42e9b5ed240f75306cd95f3cda915200edcaac
mclacore <michael@massdriver.cloud>
2023-04-25 15:47:17 -0600

    Fixes and stuff

---
+++ azure-app-service/src/_artifacts.tf
@@ -0,0 +1,25 @@
+locals {
+  endpoint = var.dns.enable_dns ? "${var.dns.subdomain}.${local.dns_zone}" : module.application.hostname
+  dns_zone = var.dns.enable_dns ? element(split("/", var.dns.zone_id), index(split("/", var.dns.zone_id), "dnszones") + 1) : null
+}
+
+resource "massdriver_artifact" "endpoint" {
+  field                = "endpoint"
+  provider_resource_id = module.application.resource_id
+  name                 = "Azure App Service endpoint for ${var.md_metadata.name_prefix}"
+  artifact = jsonencode(
+    {
+      data = {
+        api = {
+          hostname = local.endpoint
+          port     = 443
+        }
+      }
+      specs = {
+        api = {
+          version = "${var.image.name}:${var.image.tag}"
+        }
+      }
+    }
+  )
+}
