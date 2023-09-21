147b910671cc489ed8c88de2abf0423d3e0b9ce6
Cory O'Daniel <cory@coryodaniel.com>
2023-09-21 16:53:42 -0700

    RedwoodJS Beta (#126)
    * RedwoodJS Beta
    
    This is implemented as two different templates.
    
    RedwoodJS deploys an API as well as static assets to a CDN.
    
    This beta implementation runs a second kubernetes deployment as a static web server.
    
    Before public release the redwoodjs-static-kubernetes bundle will be replaced w/
    cloud CDN bundles.
    
    * rwjs templates
    
    * deployin
    
    * seed notes
    
    * rails & phoenix app alarms
    
    * Add alarm channel module
    
    ---------
    
    Co-authored-by: chrisghill <chris@massdriver.cloud>

---
+++ phoenix-kubernetes/src/monitoring.tf
@@ -0,0 +1,13 @@
+module "alarm_channel" {
+  source      = "github.com/massdriver-cloud/terraform-modules//k8s/alarm-channel?ref=41e799c"
+  md_metadata = var.md_metadata
+  namespace   = var.namespace
+}
+
+module "application_alarms" {
+  source            = "github.com/massdriver-cloud/terraform-modules//massdriver/k8s-application-alarms?ref=41e799c"
+  md_metadata       = var.md_metadata
+  deployment_alarms = true
+  hpa_alarms        = var.replicas.autoscalingEnabled
+  job_alarms        = true
+}
---
+++ redwoodjs-static-kubernetes/src/main.tf
@@ -0,0 +1,25 @@
+module "helm" {
+  source    = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=40bbc7b"
+  name      = var.md_metadata.name_prefix
+  namespace = var.namespace
+
+  chart           = "redwoodjs"
+  helm_repository = "https://massdriver-cloud.github.io/helm-charts/"
+  helm_version    = "0.1"
+
+  kubernetes_cluster = var.kubernetes_cluster
+
+  helm_additional_values = {
+    # # The default command, args, and migration settings are below. Any
+    # # values in values.yaml can be overwritten by adding a parameter to your massdriver.yaml
+    # # or by hard coding the value here
+    #     command = ["yarn"]
+    #     args    = ["foo"]    
+    args = [
+      "node_modules/.bin/rw-server", "web", "--apiHost", "https://${var.blog_api.data.api.hostname}"
+    ]
+    migration = {
+      enabled = false
+    }
+  }
+}
