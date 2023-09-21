b5ffaa196fb39012b7a778543161c03c3a117fdd
Cory O'Daniel <cory@coryodaniel.com>
2023-09-21 00:42:01 -0700

    RedwoodJS Beta
    This is implemented as two different templates.
    
    RedwoodJS deploys an API as well as static assets to a CDN.
    
    This beta implementation runs a second kubernetes deployment as a static web server.
    
    Before public release the redwoodjs-static-kubernetes bundle will be replaced w/
    cloud CDN bundles.

---
+++ redwoodjs-static-kubernetes/src/monitoring.tf
@@ -0,0 +1,6 @@
+module "application_alarms" {
+  source            = "github.com/massdriver-cloud/terraform-modules//massdriver/k8s-application-alarms?ref=40bbc7b"
+  md_metadata       = var.md_metadata
+  deployment_alarms = true
+  hpa_alarms        = var.replicas.autoscalingEnabled
+}
