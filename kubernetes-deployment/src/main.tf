8e8a1d6a895125b8924dd377bff460d287118a9b
mclacore <michael@massdriver.cloud>
2024-01-03 14:23:17 -0800

    Add pod labels in app template

--- kubernetes-deployment/src/main.tf
+++ kubernetes-deployment/src/main.tf
@@ -1,6 +1,6 @@
 
 module "helm" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=40bbc7b"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=45b7ffa"
   name               = var.md_metadata.name_prefix
   namespace          = var.namespace
   chart              = "${path.module}/chart"
