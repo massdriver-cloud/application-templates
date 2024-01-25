faea3a27f085666d1fd3be57d4e663f67de41718
Michael Lacore <michael@massdriver.cloud>
2024-01-25 15:35:01 -0800

    Update to app service template reflecting module changes (#135)
    * Update to app service template reflecting module changes
    
    * Update image name to image repository for continuity
    
    * Update description, match module
    
    * Update azure-app-service/massdriver.yaml
    
    Co-authored-by: Christopher Hill <chris@massdriver.cloud>
    
    ---------
    
    Co-authored-by: Christopher Hill <chris@massdriver.cloud>

--- azure-app-service/src/main.tf
+++ azure-app-service/src/main.tf
@@ -1,5 +1,5 @@
 module "application" {
-  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=d536e3a"
+  source             = "github.com/massdriver-cloud/terraform-modules//massdriver-application-azure-app-service?ref=c09e929"
   name               = var.md_metadata.name_prefix
   location           = var.azure_virtual_network.specs.azure.region
   application        = var.application
