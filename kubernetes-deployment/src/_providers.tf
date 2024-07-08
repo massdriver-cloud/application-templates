e59e50e1dfbfb7df542bac18429001e16581f457
mclacore <michael@massdriver.cloud>
2024-07-08 15:37:18 -0700

    Movnig module to app template repo

--- kubernetes-deployment/src/_providers.tf
+++ kubernetes-deployment/src/_providers.tf
@@ -1,11 +1,15 @@
 terraform {
   required_providers {
-    mdxc = {
-      source = "massdriver-cloud/mdxc"
+    jq = {
+      source = "massdriver-cloud/jq"
     }
 
     massdriver = {
       source = "massdriver-cloud/massdriver"
+    }
+
+    mdxc = {
+      source = "massdriver-cloud/mdxc"
     }
 
     helm = {
--- kubernetes-deployment/src/monitoring.tf
+++ kubernetes-deployment/src/monitoring.tf
@@ -1,12 +1,12 @@
 
 module "alarm_channel" {
-  source      = "github.com/massdriver-cloud/terraform-modules//k8s/alarm-channel?ref=41e799c"
+  source      = "github.com/massdriver-cloud/terraform-modules//k8s/alarm-channel?ref=a1b2019"
   md_metadata = var.md_metadata
   namespace   = var.namespace
 }
 
 module "application_alarms" {
-  source            = "github.com/massdriver-cloud/terraform-modules//massdriver/k8s-application-alarms?ref=40bbc7b"
+  source            = "github.com/massdriver-cloud/terraform-modules//massdriver/k8s-application-alarms?ref=a1b2019"
   md_metadata       = var.md_metadata
   deployment_alarms = true
   hpa_alarms        = var.replicas.autoscalingEnabled
