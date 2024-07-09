6c077216cc72a5a976d92c80626e0882ea76e8cb
mclacore <michael@massdriver.cloud>
2024-07-09 12:01:10 -0700

    fixing typo

--- gcp-cloud-function/src/_providers.tf
+++ gcp-cloud-function/src/_providers.tf
@@ -10,7 +10,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      verison = "~> 4.0"
+      version = "~> 4.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
