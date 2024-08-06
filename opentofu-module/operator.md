53e01c221333e5707763e61ee1ea4edf8f796f75
Michael Lacore <michael@massdriver.cloud>
2024-08-06 15:06:26 -0700

    Make OpenTofu template (#148)
    * copying terraform-module for opentofu
    
    * adding terraform provisioner to app template
    
    * replacing terraform svg w/ opentofu
    
    * moving steps block out of liquid

---
+++ opentofu-module/operator.md
@@ -0,0 +1,5 @@
+# Operator Guide
+
+This is the guide for your bundle. It will appear under the `guide` button in Massdriver.
+
+Use it to describe to users how the bundle works, use cases for the bundle, and examples.
---
+++ opentofu-module/src/_providers.tf
@@ -0,0 +1,80 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    massdriver = {
+      source  = "massdriver-cloud/massdriver"
+      version = "~> 1.0"
+    }
+    # utility = {
+    #   source = "massdriver-cloud/utility"
+    # }
+    # aws = {
+    #   source  = "hashicorp/aws"
+    #   version = "~> 4.0"
+    # }
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = "~> 3.0"
+    # }
+    # google = {
+    #   source  = "hashicorp/google"
+    #   version = "~> 4.0"
+    # }
+    # google-beta = {
+    #   source  = "hashicorp/google-beta"
+    #   version = "~> 4.0"
+    # }
+    # helm = {
+    #   source  = "hashicorp/helm"
+    #   version = "~> 2.0"
+    # }
+    # kubernetes = {
+    #   source  = "hashicorp/kubernetes"
+    #   version = "~> 2.0"
+    # }
+  }
+}
+
+# provider "aws" {
+#   region     = var.network.specs.aws.region
+#   assume_role {
+#     role_arn    = var.aws_authentication.data.arn
+#     external_id = var.aws_authentication.data.external_id
+#   }
+#   default_tags {
+#     tags = var.md_metadata.default_tags
+#   }
+# }
+
+# provider "azurerm" {
+#   features {}
+
+#   client_id       = var.azure_service_principal.data.client_id
+#   tenant_id       = var.azure_service_principal.data.tenant_id
+#   client_secret   = var.azure_service_principal.data.client_secret
+#   subscription_id = var.azure_service_principal.data.subscription_id
+# }
+
+# provider "google" {
+#   project     = var.gcp_authentication.data.project_id
+#   credentials = jsonencode(var.gcp_authentication.data)
+#   region      = var.mrc.specs.gcp.region
+# }
+
+# provider "google-beta" {
+#   project     = var.gcp_authentication.data.project_id
+#   credentials = jsonencode(var.gcp_authentication.data)
+#   region      = var.mrc.specs.gcp.region
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     config_context = "default"
+#     config_path = "kube.yaml"
+#   }
+# }
+
+# provider "kubernetes" {
+#   config_context = "default"
+#   config_path = "kube.yaml"
+# }
